5ad73827a62edc90664a7139f14e5b980349a931
Shohei Ikeda <Shohei@naist-wavenet127-002.naist.jp>
2016-05-06 19:46:28 +0900

    タイマーとプログレスバーを追加．

--- js/app.js
+++ js/app.js
@@ -1,34 +1,90 @@
 /*
 
 TODO
-
+タイマー部分
+　カウントダウンタイマバー（プログレスバー）の表示．
+　残り２割になったときに背景を赤に変更．
+　0になったときのクリア判定．
+アイテム部分
+　ステータス（アイテムによるLOC増加量）の表示．
+　アイテム購入ボタン．
+　アイテムによる定期LOCの追加，キータイプ時LOCの追加．
+　アイテム購入後，グラフィックの追加．
 
 */
 
-var typeStart = true;
-var codeView = $('#codeView');
-var codeMaxLength = 30;
-var loc = 0;
-var clearLoc = 100;
-var digit = 1;
+/* LOCカウント部用-------------------------------------------- */
+var typeStart = true; // ゲーム開始判定．true=まだ始まっていない．
+var codeMaxLength = 30; // codeViewクリアまでの最大行数．
+var loc = 0; // LOC．これがclearLoc分貯まればクリア．
+var clearLoc = 100; // クリアに必要なloc．
+var digit = 1; // 現在のLOC桁数．
+/* LOCカウント部用-------------------------------------------- */
+var timer;
+var endTime = 180000; // 終了までの時間．1000=1秒．
+var minute = 2;
+var second = 60;
+var bar;
 
 window.addEventListener("load", init);
 
 // 初期設定
 function init(){
+    timerSetup();
+}
 
+function timerSetup() {
+    /* progressbar部分 */
+    bar = new ProgressBar.Line(container, {
+        strokeWidth: 1, // 下のバーに対する比率．この場合上のバーは1.5倍．
+        easing: 'linear',
+        duration: endTime, // 終わるまでの時間
+        color: '#eee',
+        trailColor: '#ED6A5A',
+        trailWidth: 1,
+        svgStyle: {
+            width: '100%',
+            height: '100%'
+        },
+        from: {
+            color: '#eee' // 変化前の色
+        },
+        to: {
+            color: '#eee' // 変化後の色
+        },
+        step: (state, bar) => {
+            bar.path.setAttribute('stroke', state.color);
+        }
+    });
+}
+
+function timerStart(){
+    bar.animate(1.0);  // 0.0~1.0．0.5とかにすると半分までしか進まない．
+
+    /* タイマビュー部分 */
+    timer = setInterval(function() {
+        if(second == 0){
+            if(minute == 0){
+                alert("タイムアップ！");
+            }
+            second = 60;
+            minute--;
+        }
+        second--;
+        $('#timerView').text(("0" + String(minute)).slice(-2) + ":" + ("0" + String(second)).slice(-2));
+    }, 1000);
 }
 
 /* keyup時イベント．LOCのカウント，それに合わせたコード片の表示． */
 document.addEventListener("keyup", function(e){
     /* LOCカウント部 */
-    loc++;
+    loc++; // TODO アイテムに応じて増加量を変更．
     if(String(loc).length > digit){ // 桁数による位置調整
         $("#loc").css("left", $('#loc').position().left - 7);
         digit = String(loc).length;
     }
     $('#loc').text(loc);
-    if(loc > clearLoc-1){ // クリア判定．5分立った時にするか，ある一定以上書き上げたらにするか．後者ならlocが減っていくでもいいよな．
+    /*if(loc > clearLoc-1){ // クリア判定．5分立った時にするか，ある一定以上書き上げたらにするか．後者ならlocが減っていくでもいいよな．
         alert("クリア！");
         loc = 0;
         $('#loc').text(loc);
@@ -37,15 +93,13 @@ document.addEventListener("keyup", function(e){
         digit = 1;
         document.getElementById("codeView").value = "_";
         return;
-    }
-
-    /* Timer部 */
-    // □□とかで進捗バーみたいなものが表示されるといいよね．
+    }*/
 
     /* Code部 */
     if(typeStart){
-        document.getElementById("codeView").value = "function func(e){" + "\n";
+        document.getElementById("codeView").value = "function func(e){" + "\n" + "    var start=\"start!\"" + "\n";
         typeStart = false;
+        timerStart();
     }
 
     document.getElementById("codeView").value += randomCode(); // ランダムなコード片を追記
@@ -65,7 +119,6 @@ document.addEventListener("keyup", function(e){
 function randomCode(){
     var code = "";
     var rand = Math.floor(Math.random() * 11) ;
-    console.log(rand);
 
     switch (rand){
         case 0:
